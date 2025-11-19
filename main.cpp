ff54b8cf91af756abb09c238a72933b2c2743e9b
KamilMGk <kamillo.michalak@gmail.com>
2025-11-19 19:03:35 +0000

    kgrf

--- main.cpp
+++ main.cpp
@@ -1,38 +1,31 @@
-#include "rectangle.cpp"
+#include <iostream>
 
-int main() {
-    // Tworzenie obiektu klasy Rectangle
-    Rectangle rect(10, 5);
+using namespace std;
 
-    // Wyświetlanie informacji o prostokącie
-    rect.display();
-
-    // Pobieranie szerokości i wysokości
-    double width = rect.getWidth();
-    double height = rect.getHeight();
-    std::cout << "Width: " << width << ", Height: " << height << "\n";
-
-    // Obliczanie pola i obwodu
-    double area = rect.area();
-    double perimeter = rect.perimeter();
-    std::cout << "Area: " << area << ", Perimeter: " << perimeter << "\n";
-
-    // Zmiana szerokości i wysokości
-    rect.setWidth(15);
-    rect.setHeight(8);
-
-    // Wyświetlanie zaktualizowanych informacji o prostokącie
-    rect.display();
-
-    // Pobieranie nowej szerokości i wysokości
-    width = rect.getWidth();
-    height = rect.getHeight();
-    std::cout << "New Width: " << width << ", New Height: " << height << "\n";
-
-    // Obliczanie nowego pola i obwodu
-    area = rect.area();
-    perimeter = rect.perimeter();
-    std::cout << "New Area: " << area << ", New Perimeter: " << perimeter << "\n";
-
-    return 0;
+class Rectangle{
+    private:
+    double width;
+    double height;
+    public:
+    Rectangle(double w,double h):width(w),height(h) {}
+    void setWidth(double w){
+        width=w;
+    }
+    double getWidth()const{
+        return width;
+    }
+    double getHeight()const{
+        return height;
+    }
+    double area() const{
+        return width*height;
+    }
+    double perimeter() const{
+        return (2 * (width + height));
+    }
+void display() const{
+    cout<<"Rectanagle: "<<width<<"x"<<height<<endl;
+    cout<<"Area: "<<area()<<endl;
+    cout<<"Perimeter: "<<perimeter()<<endl;
 }
+};
