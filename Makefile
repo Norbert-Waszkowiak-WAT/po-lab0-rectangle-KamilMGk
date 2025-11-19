85ad81f9d4dcd3e5b447084e120966adec703153
KamilMGk <kamillo.michalak@gmail.com>
2025-11-19 19:03:35 +0000

    Refactor: remove header; make rectangle.cpp self-contained; update main, test and Makefile

--- Makefile
+++ Makefile
@@ -1,11 +1,17 @@
 all: clean test
 
-appTests: test.cpp rectangle.cpp
+appTests: test.cpp catch_amalgamated.cpp
 	g++ -Wall --std=c++17 test.cpp catch_amalgamated.cpp -o appTests
+
+appMain: main.cpp
+	g++ -Wall --std=c++17 main.cpp -o appMain
 
 test: appTests
 	# executes all tests
 	./appTests
 
+run: appMain
+	./appMain
+
 clean:
-	rm -f appTests
+	rm -f appTests appMain
--- main.cpp
+++ main.cpp
@@ -1,29 +1,14 @@
+#include <iostream>
 #include "rectangle.cpp"
- 
+
 int main() {
-    Rectangle rect(10, 5);
- 
+    Rectangle rect(10.0, 5.0);
     rect.display();
- 
-    double width = rect.getWidth();
-    double height = rect.getHeight();
-    std::cout << "Width: " << width << ", Height: " << height << "\n";
- 
-    double area = rect.area();
-    double perimeter = rect.perimeter();
-    std::cout << "Area: " << area << ", Perimeter: " << perimeter << "\n";
- 
-    rect.setWidth(15);
-    rect.setHeight(8);
+
+    rect.setWidth(15.0);
+    rect.setHeight(8.0);
+    std::cout << "After resize:\n";
     rect.display();
- 
-    width = rect.getWidth();
-    height = rect.getHeight();
-    std::cout << "New Width: " << width << ", New Height: " << height << "\n";
- 
-    area = rect.area();
-    perimeter = rect.perimeter();
-    std::cout << "New Area: " << area << ", New Perimeter: " << perimeter << "\n";
- 
+
     return 0;
 }
